6191f5969e56ae7348501d8030e4666f63a94e7f
Ronan <e22200662>
2026-01-27 10:49:11 +0100

    deuxieme commit

--- main.js
+++ main.js
@@ -4,6 +4,16 @@ console.log('Hello, world!');
 const operand = 12;
 const operand2 = 45;
 
-let result = operand + operand2;
+b += a ;
+console.log("resultat de a + b = " + b);
 
-console.log("resultat de" + operand + "+" + operand2 + "=" + result);
+
+let aPerson= new Object();
+aPerson.name = 'Jean';
+aPerson.age = 35;
+aPerson.gender = 'masculin';
+aPerson.interest = ['musique', 'badminton'];
+
+aPerson.sayHello = function() {console.log('Bonjour ! Je suis ' + this.name + '.'); };
+
+aPerson.sayHello();
